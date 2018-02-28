c30096d7d2bfaebe626ba017e08a874fd3705956
root <root@charli-Aspire-E5-551>
2018-02-28 00:42:38 -0600

    add content, add styles with sass. Create a new template "buy".

--- statics/js/main.js
+++ statics/js/main.js
@@ -1,15 +1,12 @@
-/*$(function() {
-  var imagenes = ['banner1.jpg', 'banner2.jpeg', 'banner3.jpg', 'banner4.JPG'];
-  $('#mainbanner').css({'background-image': 'url(statics/images/' + imagenes[Math.floor(Math.random() * imagenes.length)] + ')'});
-}); */
+$ = jQuery.noConflict();
 
 do {
 	$(document).ready(function() {
 
 	var contenedor = $('#mainbanner');
-	var imagenes = ['banner1.jpg', 'banner2.jpeg', 'banner3.jpg', 'banner4.JPG', 'banner5.jpeg', 'banner6.jpeg', 'banner7.jpeg'];
+	var imagenes = ['banner1.jpeg', 'banner2.jpeg', 'banner3.jpeg', 'banner4.jpeg', 'banner5.jpeg', 'banner6.jpeg', 'banner7.jpeg'];
 	var tiempo = 2500;
-	contenedor.css({'background-image':'url(statics/images/banner1.jpg)'});
+	contenedor.css({'background-image':'url(statics/images/banner3.jpeg)'});
 
 	function image(){
 	setTimeout(function() {
@@ -20,14 +17,32 @@ do {
 	image();
 	});
 } while (false);
-
-
-
+/*show menu*/
+$(document).ready(function(){
+	$('#show-menu').on("click", function(){
+		$('#navigation').animate({right:"0"},700);
+		$('#show-menu').css({'visibility':"hidden"});
+		$('.modal').css({'visibility':"visible"},700);
+		$('.modal').animate({opacity:"1"},700);
+	});
+});
 /*change background color*/
   $(document).scroll(function(){
-    if($(this).scrollTop() > 20) {
-      $('#navbar-main').css({"background-color":"rgba(2,4,0,0.8)"});
+    if($(this).scrollTop() > 25) {
+      $('#navbar-main').css({"background-color":"rgba(0,0,0,0.8)"});
     } else {
-      $('#navbar-main').css({"background-color":"rgba(0,0,0,0.15)"});
+      $('#navbar-main').css({"background-color":"rgba(0,0,0,0.01)"});
     }
   });
+/*menu lateral*/
+	var navigation		= document.getElementById('navigation');
+	var hammer		= new Hammer.Manager(navigation);
+	var swipe		= new Hammer.Swipe();
+
+	hammer.add(swipe);
+	hammer.on('swiperight', function(){
+	   $(navigation).animate({right: "-85%"}, 500)
+ 		 $('#show-menu').css({'visibility':"visible"});
+		 $('.modal').animate({'visibility':"hidden"},100);
+ 		$('.modal').animate({opacity:"0"},700);
+	});
